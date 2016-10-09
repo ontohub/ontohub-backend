96687c1bc7b9ae3880377bef9a9b76af0955f1ff
Eugen Kuksa <kuksa.eugen@gmail.com>
2016-10-09 16:53:00 +0200

    Add namespaces route.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,15 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :repositories, format: false,
-                           controller: 'v2/repositories',
-                           except: %i(new edit),
-                           param: :slug
+  resources :namespaces,
+    format: false,
+    controller: 'v2/namespaces',
+    only: :show,
+    param: :slug do
+      resources :repositories,
+        format: false,
+        controller: 'v2/repositories',
+        except: %i(new edit),
+        param: :slug
+    end
 end
