6be9a40b2a4781fc441edcbefb4d83ec1e94b1f2
Eugen Kuksa <kuksa.eugen@gmail.com>
2016-10-09 19:42:22 +0200

    Refactor spec_helper to maintain line coverage.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,12 +43,10 @@ RSpec.configure do |config|
   # Many RSpec users commonly either run the entire suite or an individual
   # file, and it's useful to allow more verbose output when running an
   # individual spec file.
-  if config.files_to_run.one?
-    # Use the documentation formatter for detailed output,
-    # unless a formatter has already been configured
-    # (e.g. via a command-line flag).
-    config.default_formatter = 'doc'
-  end
+  # Use the documentation formatter for detailed output,
+  # unless a formatter has already been configured
+  # (e.g. via a command-line flag).
+  config.default_formatter = 'doc' if config.files_to_run.one?
 
   # Print the 10 slowest examples and example groups at the
   # end of the spec run, to help surface which specs are running
