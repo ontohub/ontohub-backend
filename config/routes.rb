38e2bd3f988dea5eefa2eb3be6d5cf8e0017274e
Eugen Kuksa <kuksa.eugen@gmail.com>
2016-11-07 17:12:05 +0100

    Move repositories routes out of the namespace except for the index route
    This is necessary to avoid doubled namespace in the URL of a repository
    because the namespace's slug is now part of the repository's slug.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :namespaces,
     format: false,
     controller: 'v2/namespaces',
@@ -10,7 +9,15 @@ Rails.application.routes.draw do
       resources :repositories,
         format: false,
         controller: 'v2/repositories',
-        except: %i(new edit),
-        param: :slug
+        only: :index
     end
+
+  # Repositories is actually nested in namespaces, but its slug contains the
+  # namespace's slug, separated by a slash.
+  resources :repositories,
+    format: false,
+    controller: 'v2/repositories',
+    except: %i(index new edit),
+    param: :slug,
+    constraints: {slug: %r{[^/]+/[^/]+}}
 end
