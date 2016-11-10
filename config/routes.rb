e84f001b7d2e90cf23c5268d5336078ba020de05
Eugen Kuksa <kuksa.eugen@gmail.com>
2016-11-10 10:36:03 +0100

    Disable (.:format) in all routes by default.

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,22 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :namespaces,
-    format: false,
-    controller: 'v2/namespaces',
-    only: :show,
-    param: :slug do
-      resources :repositories,
-        format: false,
-        controller: 'v2/repositories',
-        only: :index
-    end
+  scope format: false do
+    resources :namespaces,
+      controller: 'v2/namespaces',
+      only: :show,
+      param: :slug do
+        resources :repositories,
+          controller: 'v2/repositories',
+          only: :index
+      end
 
-  # Repositories is actually nested in namespaces, but its slug contains the
-  # namespace's slug, separated by a slash.
-  resources :repositories,
-    format: false,
-    controller: 'v2/repositories',
-    except: %i(index new edit),
-    param: :slug,
-    constraints: {slug: %r{[^/]+/[^/]+}}
+    # Repositories is actually nested in namespaces, but its slug contains the
+    # namespace's slug, separated by a slash.
+    resources :repositories,
+      controller: 'v2/repositories',
+      except: %i(index new edit),
+      param: :slug,
+      constraints: {slug: %r{[^/]+/[^/]+}}
+  end
 end
