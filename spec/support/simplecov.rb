ad5ba7ce4446d27c3fb562025c556553067b7db8
Eugen Kuksa <kuksa.eugen@gmail.com>
2016-12-21 15:18:26 +0100

    Disable SimpleCov for rbx.

--- spec/support/simplecov.rb
+++ spec/support/simplecov.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
-unless defined?(Coveralls)
-  require 'simplecov'
-  require 'coveralls'
-  SimpleCov.formatters = [
-    SimpleCov::Formatter::HTMLFormatter,
-    Coveralls::SimpleCov::Formatter,
-  ]
-  SimpleCov.start do
-    # The schema matcher does not need to be tested.
-    add_filter 'spec/support/json_schema_matcher.rb'
+if RUBY_ENGINE == 'ruby' # not 'rbx'
+  unless defined?(Coveralls)
+    require 'simplecov'
+    require 'coveralls'
+    SimpleCov.formatters = [
+      SimpleCov::Formatter::HTMLFormatter,
+      Coveralls::SimpleCov::Formatter,
+    ]
+    SimpleCov.start do
+      # The schema matcher does not need to be tested.
+      add_filter 'spec/support/json_schema_matcher.rb'
 
-    # The config of the 'config' gem does not need to be tested.
-    add_filter 'config/initializers/config.rb'
+      # The config of the 'config' gem does not need to be tested.
+      add_filter 'config/initializers/config.rb'
+    end
   end
 end
