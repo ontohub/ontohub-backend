b9cba83d3de355cf537859164436898613f249c6
Tom Gehrke <phyrog@cognitive-coding.com>
2017-01-24 16:30:41 +0100

    Stub Version::VERSION constant in controller test

--- spec/controllers/v2/version_controller_spec.rb
+++ spec/controllers/v2/version_controller_spec.rb
@@ -4,6 +4,9 @@ require 'rails_helper'
 
 RSpec.describe V2::VersionController do
   describe 'GET show' do
+    before do
+      stub_const('Version::VERSION', '0.0.0-12-gabcdefg')
+    end
     before { get :show }
     it { expect(response).to have_http_status(:ok) }
     it { expect(response).to match_response_schema('v2', 'jsonapi') }
