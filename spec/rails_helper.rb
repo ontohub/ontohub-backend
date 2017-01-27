1a315629df198fe7dfbd0dc77daacd49eb3c99c8
Eileen Bolloff <ebolloff@informatik.uni-bremen.de>
2017-01-27 10:39:07 +0100

    Use devise in tests.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,6 +15,7 @@ require 'json-schema'
 require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'devise'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -51,4 +52,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace('gem name')
+
+  # Use Devise in tests
+  config.include Devise::Test::ControllerHelpers, type: :controller
 end
