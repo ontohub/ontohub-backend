a3513a3dbed62f5eaadb05fce40cc6ed6f7977ef
Tom Gehrke <phyrog@users.noreply.github.com>
2017-01-27 09:06:51 +0100

    Don't run version check in test env (#60)
    * Don't run version check in test env
    
    * Stub Version::VERSION constant in controller test
    
    * Add nocov comments

--- app/models/version.rb
+++ app/models/version.rb
@@ -35,19 +35,29 @@ class Version < ActiveModelSerializers::Model
 
     private
 
+    # :nocov:
     def production?
       Rails.env.production?
     end
+    # :nocov:
 
+    def test?
+      Rails.env.test?
+    end
+
+    # :nocov:
     def read_version_file
       File.read(Rails.root.join('VERSION')).strip
     rescue Errno::ENOENT
       ''
     end
+    # :nocov:
 
+    # :nocov:
     def read_version_from_git
       `git describe --long --tags`.strip
     end
+    # :nocov:
 
     def exception_message
       msg = 'Could not determine the backend version. '
@@ -61,5 +71,5 @@ class Version < ActiveModelSerializers::Model
     end
   end
 
-  VERSION = load_version
+  VERSION = load_version unless test?
 end
