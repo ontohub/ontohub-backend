6b8587bc9d96d29e92498c5e18882518c382b60a
Eugen Kuksa <kuksa.eugen@gmail.com>
2017-02-19 16:03:49 +0100

    Add git instances and files in the seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,8 @@ end
 url_path_method = lambda do |resource|
   V2::OrganizationsController.resource_url_path(resource)
 end
-Organization.new(name: 'All Users', url_path_method: url_path_method).save
+Organization.new(name: 'Seed User Organization',
+                 url_path_method: url_path_method).save
 organization = Organization.first
 User.all do |user|
   organization.add_member(user)
@@ -28,11 +29,24 @@ url_path_method = lambda do |repository|
 end
 owner_count = OrganizationalUnit.count
 content_types = %w(ontology model specification mathematical)
-(0..(2 * owner_count - 1)).each do |index|
-  Repository.new(owner: OrganizationalUnit.find(id: index % owner_count + 1),
-                 name: "repo#{index}",
-                 content_type: content_types[index % content_types.size],
-                 public_access: true,
-                 description: 'This is a dummy repository.',
-                 url_path_method: url_path_method).save
+(0..(2 * owner_count - 1)).each do |repo_index|
+  owner = OrganizationalUnit.find(id: repo_index % owner_count + 1)
+  repository =
+    RepositoryCompound.
+      new(owner: owner,
+          name: "repo#{repo_index}",
+          content_type: content_types[repo_index % content_types.size],
+          public_access: true,
+          description: 'This is a dummy repository.',
+          url_path_method: url_path_method)
+  repository.save
+
+  user = owner.is_a?(Organization) ? owner.members.first : owner
+  (1..5).each do |file_index|
+    path = "#{file_index}_test.txt"
+    path = "subdir_#{file_index}/#{path}" if file_index <= 2
+    Blob.new(repository: repository, user: user, branch: 'master', path: path,
+             content: "test file ##{file_index}", encoding: 'plain',
+             commit_message: "Add #{path}.").create
+  end
 end
