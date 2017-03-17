1897575010a382b560609c1043acf6db6fd30604
Eugen Kuksa <kuksa.eugen@gmail.com>
2017-03-17 15:57:28 +0100

    Monkey-patch Rails to not remove double slashes in the routes as soon as the routes have been loaded.

--- config/routes.rb
+++ config/routes.rb
@@ -56,9 +56,36 @@ Rails.application.routes.draw do
         action: :show
     end
 
+    # Repositories
+    resources '/',
+      param: :slug,
+      controller: 'v2/repositories',
+      except: %i(index new create edit),
+      constraints: {slug: CONTAINING_ONE_SLASH}
+
+    # Below Repositories
+    scope '/:repository_slug',
+      constraints: {repository_slug: CONTAINING_ONE_SLASH} do
+        routes_under_repository.call
+        # by reference
+        scope '/ref/:ref' do
+          routes_under_repository.call
+        end
+      end
+
+    # Easy access to Organizational Units
     get '/:slug',
       controller: 'v2/organizational_units',
       action: :show,
       as: :organizational_unit
   end
+
+  # The router normalizes the paths of all routes and of all requests. This
+  # also removes doubles slashes. That removal needs to be suppressed for the
+  # requests because our URLs use double and even triple slashes. The following
+  # flag activates a monkey patch after the routes have been loaded. It must
+  # not be active during route loading because the path normalization must
+  # happen then.
+  # rubocop:disable Style/GlobalVars
+  $do_not_merge_multiple_slashes_in_request_urls = true
 end
