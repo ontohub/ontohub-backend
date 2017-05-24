eb756e645bbd2fe05c988cf38867fb4bb385de6f
Eugen Kuksa <kuksa.eugen@gmail.com>
2017-05-24 11:29:12 +0200

    Move some devise config to the models.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -173,12 +173,12 @@ Devise.setup do |config|
 
   # ==> Configuration for :validatable
   # Range for password length.
-  config.password_length = 10..128
+  # config.password_length = 10..128
 
   # Email regex used to validate email formats. It simply asserts that one (and
   # only one) @ exists in the given string. This is mainly to give user
   # feedback and not to assert the e-mail validity.
-  config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
+  # config.email_regexp = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
 
   # ==> Configuration for :timeoutable The time you want to timeout the user
   # session without activity. After this time the user will be asked for
