dff75ab0ec80e9cfc642becb24c4ed5f5be3c3bc
Tom Gehrke <phyrog@cognitive-coding.com>
2017-06-16 13:13:31 +0200

    Add field descriptions

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,7 +1,7 @@
 Types::QueryType = GraphQL::ObjectType.define do
   name 'Query'
 
-  field :version, Types::VersionType do
+  field :version, !Types::VersionType do
     description 'The version of the backend'
     resolve(lambda do |_obj, _args, _ctx|
       Version.new(Version::VERSION)
