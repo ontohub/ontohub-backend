0cca288af85aedfee151a9711e3e10a5cc30ca54
Tom Gehrke <phyrog@cognitive-coding.com>
2017-07-07 18:35:11 +0200

    Fix copy paste mistakes

--- app/graphql/mutations/save_repository_mutation.rb
+++ app/graphql/mutations/save_repository_mutation.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Mutations
-  # GraphQL mutation to update an organization
+  # GraphQL mutation to update an repository
   class SaveRepositoryMutation
     def call(repository, arguments, _context)
       params = arguments[:data].to_h.compact
