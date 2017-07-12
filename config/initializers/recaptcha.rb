535bd3b3aa5d608af712535fe9e064674f104cca
Eugen Kuksa <kuksa.eugen@gmail.com>
2017-07-12 11:44:34 +0200

    Fix recaptcha config (#147)
    * Update gem: gitlab_git.
    
    * Update gem: ontohub-models.
    
    * Update gem: recaptcha.
    
    * Fix recaptcha config.

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Recaptcha.configure do |config|
-  config.site_key = Rails.application.secrets.recaptcha['site_key']
-  config.secret_key = Rails.application.secrets.recaptcha['secret_key']
+  config.site_key = Rails.application.secrets.recaptcha[:site_key]
+  config.secret_key = Rails.application.secrets.recaptcha[:secret_key]
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
 end
