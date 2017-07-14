771a6c667dfcf77d920b7302f9bab479b8b13cae
Eileen Bolloff <ebolloff@informatik.uni-bremen.de>
2017-07-14 11:03:56 +0200

    Add policy and tests for Repository.

---
+++ app/policies/repository_policy.rb
@@ -0,0 +1,39 @@
+class RepositoryPolicy < ApplicationPolicy
+  attr_reader :current_user, :repository
+
+  def initialize(current_user, repository)
+    @current_user = current_user
+    @repository = repository
+    super
+  end
+
+  def show?
+    repository.public_access == true ||
+      !!current_user&.accessible_repositories&.map(&:to_param)&.include?(repository.to_param)
+  end
+
+  def create?(owner)
+    if owner.is_a?(User)
+      owner&.id == current_user&.id if current_user
+    elsif owner.is_a?(Organization)
+      !OrganizationMembership.find(member: current_user, organization: owner,                                role: 'admin').nil?
+    else
+      false
+    end
+  end
+
+  def update?
+    return false unless current_user
+    create?(repository.owner) || !RepositoryMembership.
+      find(member: current_user, repository: repository,
+           role: 'admin').nil?
+  end
+
+  def index?
+    true
+  end
+
+  def destroy?
+    update?
+  end
+end
---
+++ spec/policies/repository_policy_spec.rb
@@ -0,0 +1,384 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe RepositoryPolicy do
+  context 'show?' do
+    context 'public repository' do
+      let(:repository) { create :repository, public_access: true }
+
+      context 'signed in' do
+        let(:current_user) { create :user }
+        subject { RepositoryPolicy.new(current_user, repository) }
+
+        it 'should allow to show the repository' do
+          expect(subject.show?).to be(true)
+        end
+      end
+
+      context 'signed in as admin' do
+        let(:current_user) { create :user, :admin }
+        subject { RepositoryPolicy.new(current_user, repository) }
+
+        it 'should allow to show the repository' do
+          expect(subject.show?).to be(true)
+        end
+      end
+
+      context 'not signed in' do
+        let(:current_user) { nil }
+        subject { RepositoryPolicy.new(current_user, repository) }
+
+        it 'should allow to show the repository' do
+          expect(subject.show?).to be(true)
+        end
+      end
+    end
+
+    context 'private repository' do
+      let!(:repository) { create :repository, public_access: false }
+
+      context 'not signed in' do
+        let(:current_user) { nil }
+        subject { RepositoryPolicy.new(current_user, repository) }
+
+        it 'should not allow to show the repository' do
+          expect(subject.show?).to be(false)
+        end
+      end
+
+      context 'signed in as admin' do
+        let(:current_user) { create :user, :admin }
+        subject { RepositoryPolicy.new(current_user, repository) }
+
+        it 'should allow to show the repository' do
+          expect(subject.show?).to be(true)
+        end
+      end
+
+      context 'signed in as user without access' do
+        let(:current_user) { create :user }
+        subject { RepositoryPolicy.new(current_user, repository) }
+
+        it 'should not allow to show the repository' do
+          expect(subject.show?).to be(false)
+        end
+      end
+
+      context 'signed in as user with access' do
+        context 'by ownership' do
+          let(:current_user) { create :user }
+          let(:repository_by_user) { create :repository,
+            owner: current_user }
+          subject { RepositoryPolicy.new(current_user, repository_by_user) }
+
+          it 'should allow to show the repository' do
+            expect(subject.show?).to be(true)
+          end
+        end
+
+        context 'by repository membership' do
+          let(:current_user) { create :user }
+          subject { RepositoryPolicy.new(current_user, repository) }
+
+          %w(admin write read).each do |role|
+            it "with role #{role} should allow to show the repository" do
+              repository.add_member(current_user, role)
+              expect(subject.show?).to be(true)
+            end
+          end
+        end
+
+        context 'by organization membership' do
+          let(:current_user) { create :user }
+          let(:organization) { create :organization }
+          let(:repository_by_organization) { create :repository,
+            owner: organization }
+          subject { RepositoryPolicy.new(current_user,
+            repository_by_organization) }
+
+          %w(admin write read).each do |role|
+            it "with role #{role} should allow to show the repository" do
+              organization.add_member(current_user, role)
+              expect(subject.show?).to be(true)
+            end
+          end
+        end
+      end
+    end
+  end
+
+  context 'create?' do
+    let(:repository) { create :repository, public_access: true }
+
+    context 'owner is current user' do
+      context 'not signed in' do
+        let(:current_user) { nil }
+        subject { RepositoryPolicy.new(current_user, repository) }
+
+        it 'should not allow to create the repository' do
+          expect(subject.create?(current_user)).to be(false)
+        end
+      end
+
+      context 'signed in' do
+        context 'as admin role' do
+          let(:current_user) { create :user, :admin }
+          subject { RepositoryPolicy.new(current_user, repository) }
+
+          it 'should allow to create the repository' do
+            expect(subject.create?(current_user)).to be(true)
+          end
+        end
+
+        context 'as user role' do
+          let(:current_user) { create :user }
+          subject { RepositoryPolicy.new(current_user, repository) }
+
+          it 'should allow to create the repository' do
+            expect(subject.create?(current_user)).to be(true)
+          end
+        end
+      end
+    end
+
+    context 'owner is organization' do
+      let(:current_user) { create :user }
+      let(:organization) { create :organization }
+      subject { RepositoryPolicy.new(current_user, Repository) }
+
+      %w(write read).each do |role|
+        it "with role #{role} should not allow to create the repository" do
+          organization.add_member(current_user, role)
+          expect(subject.create?(organization)).to be(false)
+        end
+      end
+
+      context 'as an organization admin' do
+        it 'should allow to create the repository' do
+          organization.add_member(current_user, 'admin')
+          expect(subject.create?(organization)).to be(true)
+        end
+      end
+
+      context 'unrelated to the organization' do
+        it 'should not allow to create the repository' do
+          expect(subject.create?(organization)).to be(false)
+        end
+      end
+    end
+  end
+
+  context 'update?' do
+    [true, false].each do |public|
+      context "with public access #{public}" do
+        let(:repository) { create :repository, public_access: public }
+
+        context 'not signed in' do
+          let(:current_user) { nil }
+          subject { RepositoryPolicy.new(current_user, repository) }
+
+          it 'should not allow to update the repository' do
+            expect(subject.update?).to be(false)
+          end
+        end
+
+        context 'signed in' do
+          context 'as admin role' do
+            let(:current_user) { create :user, :admin }
+            subject { RepositoryPolicy.new(current_user, repository) }
+
+            it 'should allow to update the repository' do
+              expect(subject.update?).to be(true)
+            end
+          end
+
+          context 'as user role without access' do
+            let(:current_user) { create :user }
+            subject { RepositoryPolicy.new(current_user, repository) }
+
+            it 'should not allow to update the repository' do
+              expect(subject.update?).to be(false)
+            end
+          end
+
+          context 'as a user role with access' do
+            context 'by ownership' do
+              let(:current_user) { create :user }
+              let(:repository_by_user) { create :repository,
+                owner: current_user }
+              subject { RepositoryPolicy.new(current_user, repository_by_user) }
+
+              it 'should allow to update the repository' do
+                expect(subject.update?).to be(true)
+              end
+            end
+
+            context 'by repository membership' do
+              let(:current_user) { create :user }
+              subject { RepositoryPolicy.new(current_user, repository) }
+
+              %w(write read).each do |role|
+                it "with role #{role} should not allow to update the repository" do
+                  repository.add_member(current_user, role)
+                  expect(subject.update?).to be(false)
+                end
+              end
+
+              it "with role admin should allow to update the repository" do
+                repository.add_member(current_user, 'admin')
+                expect(subject.update?).to be(true)
+              end
+            end
+
+            context 'by organization membership' do
+              let(:current_user) { create :user }
+              let(:organization) { create :organization }
+              let(:repository_by_organization) { create :repository,
+                owner: organization }
+              subject { RepositoryPolicy.new(current_user,
+                repository_by_organization) }
+
+              %w(write read).each do |role|
+                it "with role #{role} should not allow to update the repository" do
+                  organization.add_member(current_user, role)
+                  expect(subject.update?).to be(false)
+                end
+              end
+
+              it 'with role admin should allow to update the repository' do
+                organization.add_member(current_user, 'admin')
+                expect(subject.update?).to be(true)
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+
+  context 'index?' do
+    [true, false].each do |public|
+      context "with public access #{public}" do
+        let(:repository) { create :repository, public_access: public }
+
+        context 'not signed in' do
+          let(:current_user) { nil }
+          subject { RepositoryPolicy.new(current_user, repository) }
+
+          it 'should allow to index the repository' do
+            expect(subject.index?).to be(true)
+          end
+        end
+
+        context 'signed in' do
+          context 'as user role' do
+            let(:current_user) { create :user }
+            subject { RepositoryPolicy.new(current_user, repository) }
+
+            it 'should allow to index the repository' do
+              expect(subject.index?).to be(true)
+            end
+          end
+
+          context 'as admin role' do
+            let(:current_user) { create :user, :admin }
+            subject { RepositoryPolicy.new(current_user, repository) }
+
+            it 'should allow to index the repository' do
+              expect(subject.index?).to be(true)
+            end
+          end
+        end
+      end
+    end
+  end
+
+  context 'destroy?' do
+    [true, false].each do |public|
+      context "with public access #{public}" do
+        let(:repository) { create :repository, public_access: public }
+
+        context 'not signed in' do
+          let(:current_user) { nil }
+          subject { RepositoryPolicy.new(current_user, repository) }
+
+          it 'should not allow to destroy the repository' do
+            expect(subject.destroy?).to be(false)
+          end
+        end
+
+        context 'signed in' do
+          context 'as admin role' do
+            let(:current_user) { create :user, :admin }
+            subject { RepositoryPolicy.new(current_user, repository) }
+
+            it 'should allow to destroy the repository' do
+              expect(subject.destroy?).to be(true)
+            end
+          end
+
+          context 'as user role without access' do
+            let(:current_user) { create :user }
+            subject { RepositoryPolicy.new(current_user, repository) }
+
+            it 'should not allow to destroy the repository' do
+              expect(subject.destroy?).to be(false)
+            end
+          end
+
+          context 'as a user role with access' do
+            context 'by ownership' do
+              let(:current_user) { create :user }
+              let(:repository_by_user) { create :repository,
+                owner: current_user }
+              subject { RepositoryPolicy.new(current_user, repository_by_user) }
+
+              it 'should allow to destroy the repository' do
+                expect(subject.destroy?).to be(true)
+              end
+            end
+
+            context 'by repository membership' do
+              let(:current_user) { create :user }
+              subject { RepositoryPolicy.new(current_user, repository) }
+
+              %w(write read).each do |role|
+                it "with role #{role} should not allow to destroy the repository" do
+                  repository.add_member(current_user, role)
+                  expect(subject.destroy?).to be(false)
+                end
+              end
+
+              it "with role admin should allow to destroy the repository" do
+                repository.add_member(current_user, 'admin')
+                expect(subject.destroy?).to be(true)
+              end
+            end
+
+            context 'by organization membership' do
+              let(:current_user) { create :user }
+              let(:organization) { create :organization }
+              let(:repository_by_organization) { create :repository,
+                owner: organization }
+              subject { RepositoryPolicy.new(current_user,
+                repository_by_organization) }
+
+              %w(write read).each do |role|
+                it "with role #{role} should not allow to destroy the repository" do
+                  organization.add_member(current_user, role)
+                  expect(subject.destroy?).to be(false)
+                end
+              end
+
+              it 'with role admin should allow to destroy the repository' do
+                organization.add_member(current_user, 'admin')
+                expect(subject.destroy?).to be(true)
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
