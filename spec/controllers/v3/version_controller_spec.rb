75de966c6917696481eae3b0b811b90dac4f1976
Eugen Kuksa <kuksa.eugen@gmail.com>
2017-07-21 16:46:45 +0200

    Add comply_with_rest_api matcher.

--- spec/controllers/v3/version_controller_spec.rb
+++ spec/controllers/v3/version_controller_spec.rb
@@ -3,13 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe V3::VersionController do
-  describe 'GET show' do
+  describe 'action: show' do
     before do
       Version # rubocop:disable Lint/Void
       stub_const('Version::VERSION', '0.0.0-12-gabcdefg')
     end
     before { get :show }
     it { expect(response).to have_http_status(:ok) }
-    it { |example| expect([example, response]).to comply_with_api(nil, false) }
+    it { |example| expect([example, response]).to comply_with_rest_api }
   end
 end
--- spec/support/json_schema_matcher.rb
+++ spec/support/json_schema_matcher.rb
@@ -58,3 +58,45 @@ RSpec::Matchers.define :comply_with_api do |schema = nil, verify_jsonapi = true|
     "#{header}\n#{errors.join("\n")}"
   end
 end
+
+def normalized_rest_controller(example)
+  example.example_group.controller_class.to_s.
+    gsub('::', '/').underscore
+end
+
+# finds the innermost context and transforms it from "GET show" to "get_show".
+def normalized_rest_controller_action(example)
+  example_group = example.example_group
+  until example_group.description =~ /\Aaction:\s+/i
+    example_group = example_group.parent
+  end
+  example_group&.description&.sub(/action:\s+/, '')&.parameterize&.underscore
+end
+
+def validate_rest_controller_action(response, schema_root, controller, action)
+  schema_path = "#{schema_root}/controllers/#{controller}.json"
+  JSON::Validator.fully_validate(schema_path, response.body,
+                                 strict: false,
+                                 validateschema: true)
+  # TODO: uncomment this as soon as fragments work
+                                 # fragment: "#/definitions/actions/#{action}")
+end
+
+RSpec::Matchers.define :comply_with_rest_api do
+  errors = []
+  match do |data|
+    example, response = data
+    controller = normalized_rest_controller(example)
+    action = normalized_rest_controller_action(example)
+    schema_root = "#{Rails.root}/spec/support/api/schemas"
+
+    errors =
+      validate_rest_controller_action(response, schema_root, controller, action)
+
+    errors.empty?
+  end
+  failure_message do
+    header = "Found #{errors.size} JSON schema violations:"
+    "#{header}\n#{errors.join("\n")}"
+  end
+end
