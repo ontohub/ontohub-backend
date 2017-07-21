afdbc2be52614672bd940e603ee0315be02ac214
Eileen Bolloff <ebolloff@informatik.uni-bremen.de>
2017-07-21 14:33:29 +0200

    Remove unnecessary application policy methods.

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -14,49 +14,4 @@ class ApplicationPolicy
       end
     end
   end
-
-  def index?
-    false
-  end
-
-  def show?
-    scope.where(:id => record.id).exists?
-  end
-
-  def create?
-    false
-  end
-
-  def new?
-    create?
-  end
-
-  def update?
-    false
-  end
-
-  def edit?
-    update?
-  end
-
-  def destroy?
-    false
-  end
-
-  def scope
-    Pundit.policy_scope!(user, record.class)
-  end
-
-  class Scope
-    attr_reader :user, :scope
-
-    def initialize(user, scope)
-      @user = user
-      @scope = scope
-    end
-
-    def resolve
-      scope
-    end
-  end
 end
