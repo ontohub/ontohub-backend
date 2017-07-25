2762ec48ff15e69607481bec408310dfa2f049f5
Tom Gehrke <phyrog@cognitive-coding.com>
2017-07-25 12:26:46 +0200

    Split validation errors

--- app/graphql/instrumenters/validation_error_instrumenter.rb
+++ app/graphql/instrumenters/validation_error_instrumenter.rb
@@ -4,14 +4,23 @@ module Instrumenters
   # Rescues Sequel::ValidationFailed errors and raises
   # GraphQL::ExecutionError instead
   class ValidationErrorInstrumenter
+    # rubocop:disable Metrics/MethodLength
     def instrument(_type, field)
+      # rubocop:enable Metrics/MethodLength
       old_resolve = field.resolve_proc
       field.redefine do
         resolve(lambda do |root, arguments, context|
           begin
             old_resolve.call(root, arguments, context)
           rescue Sequel::ValidationFailed => error
-            context.add_error(GraphQL::ExecutionError.new(error.message))
+            error.errors.each do |field_name, errors|
+              errors.each do |message|
+                context.add_error(
+                  GraphQL::ExecutionError.new("#{field_name} #{message}")
+                )
+              end
+            end
+            nil
           end
         end)
       end
