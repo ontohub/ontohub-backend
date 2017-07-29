7999e894dbb3023ba5411402ee7d38bfb80ddcc4
Eugen Kuksa <kuksa.eugen@gmail.com>
2017-07-29 12:07:49 +0200

    Add mutation to delete branches.

---
+++ spec/graphql/mutations/git/delete_branch_mutation_spec.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'deleteBranch mutation' do
+  let!(:repository) { create :repository_compound }
+  let(:name) { "feature_#{Faker::Internet.user_name}" }
+  let(:name_argument) { name }
+  let(:revision) { 'master' }
+
+  let(:context) {}
+  let(:variables) do
+    {'repositoryId' => repository.to_param,
+     'name' => name_argument,
+     'revision' => revision}
+  end
+
+  let(:result) do
+    OntohubBackendSchema.execute(
+      query_string,
+      context: context,
+      variables: variables
+    )
+  end
+
+  let(:query_string) do
+    <<-QUERY
+    mutation ($repositoryId: ID!, $name: String!) {
+      deleteBranch(repositoryId: $repositoryId, name: $name)
+    }
+    QUERY
+  end
+
+  subject { result }
+
+  before do
+    repository.git.create_branch(name, revision)
+  end
+
+  context 'Successful deletion' do
+    it 'returns true' do
+      expect(subject['data']['deleteBranch']).to be(true)
+    end
+
+    it 'deletes the branch' do
+      subject
+      expect(repository.git.branch_names).not_to include(name)
+    end
+  end
+
+  context 'Unsuccessful' do
+    let(:name_argument) { "not_the_#{name}" }
+    it 'returns true' do
+      expect(subject['data']['deleteBranch']).to be(true)
+    end
+
+    it 'returns no error' do
+      expect(subject['errors']).to be(nil)
+    end
+
+    it 'does not delete the branch' do
+      subject
+      expect(repository.git.branch_names).to include(name)
+    end
+  end
+end
