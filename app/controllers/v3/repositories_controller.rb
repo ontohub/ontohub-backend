a61c0b110a8c75220c30cae8b525eeda41c2f6ef
Daniel Hackbarth <daniel-hackbarth@web.de>
2017-08-18 09:52:15 +0200

    Create GraphQl-REST controller for repositories (#182)
    * Create GraphQl-REST controller for repositories
    
    * Obey hound
    
    * obey review

---
+++ app/controllers/v3/repositories_controller.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+module V3
+  # Handles requests for version show operations
+  class RepositoriesController < V3::ApplicationController
+    graphql :index do
+      arguments do
+        {'limit' => params[:limit]&.to_i,
+         'skip' => params[:skip]&.to_i,
+         'id' => params[:organizational_unit_id]}
+      end
+      query <<-QUERY
+        query ($limit: Int, $skip: Int, $id: ID!) {
+          organizationalUnit(id: $id) {
+            id
+            repositories(limit: $limit, skip: $skip) {
+              id
+              name
+              defaultBranch
+              branches
+              contentType
+              description
+              visibility
+              owner {
+                id
+              }
+            }
+          }
+        }
+      QUERY
+    end
+    graphql :show do
+      arguments do
+        organizational_unit_id = params[:organizational_unit_id]
+        repository_id = params[:repository_id]
+        {'id' => "#{organizational_unit_id}/#{repository_id}"}
+      end
+      query <<-QUERY
+        query ($id: ID!) {
+          repository(id: $id) {
+            id
+            name
+            defaultBranch
+            branches
+            contentType
+            description
+            visibility
+            owner {
+              id
+            }
+          }
+        }
+      QUERY
+    end
+  end
+end
---
+++ spec/controllers/v3/repositories_controller_spec.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe V3::RepositoriesController do
+  subject { create :repository_compound }
+  let!(:user) { subject.owner }
+
+  context 'successful' do
+    describe 'action: index' do
+      before do
+        get :index, params: {organizational_unit_id: user.to_param}
+      end
+
+      it { expect(response).to have_http_status(:ok) }
+      it { |example| expect([example, response]).to comply_with_rest_api }
+
+      it 'finds the organizational unit' do
+        expect(response_data['organizationalUnit']).not_to be(nil)
+      end
+      it 'finds the repository' do
+        expect(response_data['organizationalUnit']['repositories']).
+          not_to be(nil)
+      end
+    end
+
+    describe 'action: show' do
+      before do
+        get :show,
+          params: {organizational_unit_id: user.to_param,
+                   repository_id: subject.to_param.sub(%r{\A[^/]*/}, '')}
+      end
+
+      it { expect(response).to have_http_status(:ok) }
+      it { |example| expect([example, response]).to comply_with_rest_api }
+
+      it 'finds the repository' do
+        expect(response_data['repository']).not_to be(nil)
+      end
+    end
+  end
+
+  context 'failing because of bad params' do
+    describe 'action: index' do
+      before do
+        get :index, params: {organizational_unit_id: ''}
+      end
+      it 'does not find the organizational unit' do
+        expect(response_data['organizationalUnit']).to be(nil)
+      end
+      it { expect(response).to have_http_status(:ok) }
+      it { |example| expect([example, response]).to comply_with_rest_api }
+    end
+
+    describe 'action: show' do
+      before do
+        get :show, params: {organizational_unit_id: '', repository_id: ''}
+      end
+      it 'does not find the repository' do
+        expect(response_data['repository']).to be(nil)
+      end
+      it { expect(response).to have_http_status(:ok) }
+      it { |example| expect([example, response]).to comply_with_rest_api }
+    end
+  end
+end
