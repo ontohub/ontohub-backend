79b5edd7b7797a22d16982f557bd1fb30b44c116
Eugen Kuksa <kuksa.eugen@gmail.com>
2017-09-01 11:32:39 +0200

    Add sign in tests.

--- spec/controllers/graphql_controller_spec.rb
+++ spec/controllers/graphql_controller_spec.rb
@@ -8,63 +8,130 @@ RSpec.describe GraphqlController do
     Version # rubocop:disable Lint/Void
     stub_const('Version::VERSION', '0.0.0-12-gabcdefg')
   end
-  let!(:user) { create :user }
 
-  let(:version_query) do
-    <<-QUERY
-    query VersionQuery {
-      version {
-        full
+  context 'not signed in' do
+    let!(:user) { create :user }
+
+    let(:version_query) do
+      <<-QUERY
+      query VersionQuery {
+        version {
+          full
+        }
       }
-    }
-    QUERY
-  end
-  let(:user_query) do
-    <<-QUERY
-    query UserQuery($id: ID!) {
-      organizationalUnit(id: $id) {
-        displayName
+      QUERY
+    end
+    let(:user_query) do
+      <<-QUERY
+      query UserQuery($id: ID!) {
+        organizationalUnit(id: $id) {
+          displayName
+        }
       }
-    }
-    QUERY
+      QUERY
+    end
+
+    let(:result) { JSON.parse(response.body) }
+
+    before do
+      post :execute,
+            params: params
+    end
+
+    context 'single query' do
+      let(:params) do
+        {query: user_query,
+         variables: {'id' => user.to_param}}
+      end
+
+      it { expect(response).to have_http_status(:ok) }
+      it 'returns the queried user data' do
+        display_name = result.dig('data', 'organizationalUnit', 'displayName')
+        expect(display_name).to eq(user.display_name)
+      end
+    end
+
+    context 'multiplexed queries' do
+      let(:params) do
+        {_json: [{query: user_query,
+                  variables: {'id' => user.to_param}},
+                 {query: version_query}],
+         graphql: true}
+      end
+
+      it { expect(response).to have_http_status(:ok) }
+      it 'returns the queried user data' do
+        display_name =
+          result.dig(0, 'data', 'organizationalUnit', 'displayName')
+        expect(display_name).to eq(user.display_name)
+      end
+      it 'returns the queried version data' do
+        full_version = result.dig(1, 'data', 'version', 'full')
+        expect(full_version).to eq(Version::VERSION)
+      end
+    end
   end
 
-  let(:result) { JSON.parse(response.body) }
+  context 'signed in' do
+    let(:me_query) do
+      <<-QUERY
+      query {
+        me {
+          id
+        }
+      }
+      QUERY
+    end
 
-  before do
-    post :execute,
+    let(:params) do
+      {query: me_query}
+    end
+
+    let(:result) { JSON.parse(response.body) }
+
+    context 'using the devise helper' do
+      create_user_and_sign_in
+      before do
+        post :execute,
           params: params
-  end
+      end
 
-  context 'single query' do
-    let(:params) do
-      {query: user_query,
-       variables: {'id' => user.to_param}}
+      it { expect(response).to have_http_status(:ok) }
+      it 'returns the queried user data' do
+        # There is only one user created by create_user_and_sign_in
+        user = User.first
+        expect(result.dig('data', 'me', 'id')).to eq(user.to_param)
+      end
     end
 
-    it { expect(response).to have_http_status(:ok) }
-    it 'returns the queried user data' do
-      display_name = result.dig('data', 'organizationalUnit', 'displayName')
-      expect(display_name).to eq(user.display_name)
-    end
-  end
+    context 'setting the token explicitly' do
+      context 'with a valid token' do
+        let!(:user) { create_user_and_set_token_header }
 
-  context 'multiplexed queries' do
-    let(:params) do
-      {_json: [{query: user_query,
-                variables: {'id' => user.to_param}},
-               {query: version_query}],
-       graphql: true}
-    end
+        before do
+          post :execute,
+            params: params
+        end
 
-    it { expect(response).to have_http_status(:ok) }
-    it 'returns the queried user data' do
-      display_name = result.dig(0, 'data', 'organizationalUnit', 'displayName')
-      expect(display_name).to eq(user.display_name)
-    end
-    it 'returns the queried version data' do
-      full_version = result.dig(1, 'data', 'version', 'full')
-      expect(full_version).to eq(Version::VERSION)
+        it { expect(response).to have_http_status(:ok) }
+        it 'returns the queried user data' do
+          expect(result.dig('data', 'me', 'id')).to eq(user.to_param)
+        end
+      end
+
+      context 'with an invalid token' do
+        before do
+          request.env['HTTP_AUTHORIZATION'] = 'Bearer ThisIsAnInvalidToken'
+          post :execute,
+            params: params
+        end
+
+        it { expect(response).to have_http_status(:unauthorized) }
+        it 'returns the queried user data' do
+          expect(response.body).
+            to eq('You need to sign in or sign up before continuing.')
+        end
+      end
     end
   end
 end
