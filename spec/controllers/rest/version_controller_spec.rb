ffa2769446a93039eff8a41506ece027c47dca00
Eugen Kuksa <kuksa.eugen@gmail.com>
2017-09-23 16:44:25 +0200

    Add means to respond with plain text in REST controllers.

--- spec/controllers/rest/version_controller_spec.rb
+++ spec/controllers/rest/version_controller_spec.rb
@@ -8,8 +8,74 @@ RSpec.describe Rest::VersionController do
       Version # rubocop:disable Lint/Void
       stub_const('Version::VERSION', '0.0.0-12-gabcdefg')
     end
-    before { get :show }
-    it { expect(response).to have_http_status(:ok) }
-    it { |example| expect([example, response]).to comply_with_rest_api }
+
+    context 'json' do
+      before { get :show }
+      it { expect(response).to have_http_status(:ok) }
+      it { |example| expect([example, response]).to comply_with_rest_api }
+    end
+
+    context 'Accept header' do
+      context 'only with json' do
+        before do
+          request.headers['Accept'] = 'application/json'
+          get :show
+        end
+
+        it { expect(response).to have_http_status(:ok) }
+        it 'does not respond with the plain text version' do
+          expect(response_data.dig('version', 'full')).to eq(Version::VERSION)
+        end
+      end
+
+      context 'only with plain' do
+        before do
+          request.headers['Accept'] = 'text/plain'
+          get :show
+        end
+
+        it { expect(response).to have_http_status(:ok) }
+        it 'responds with the plain text version' do
+          expect(response.body).to eq(Version::VERSION)
+        end
+      end
+
+      context 'with a quality parameter and a weaker alternative' do
+        before do
+          request.headers['Accept'] = 'application/json; q=0.1, text/plain; q=1'
+          get :show
+        end
+
+        it { expect(response).to have_http_status(:ok) }
+        it 'responds with the plain text version' do
+          expect(response.body).to eq(Version::VERSION)
+        end
+      end
+
+      context 'with a quality parameter and a weaker alternative in reverse '\
+        'order' do
+        before do
+          request.headers['Accept'] = 'text/plain; q=1, application/json; q=0.1'
+          get :show
+        end
+
+        it { expect(response).to have_http_status(:ok) }
+        it 'responds with the plain text version' do
+          expect(response.body).to eq(Version::VERSION)
+        end
+      end
+
+      context 'with a quality parameter and a stronger alternative' do
+        before do
+          request.headers['Accept'] = 'application/json; q=1, text/plain; q=0.1'
+          get :show
+        end
+
+        it { expect(response).to have_http_status(:ok) }
+        it 'does not respond with the plain text version' do
+          expect(response_data.dig('version', 'full')).to eq(Version::VERSION)
+        end
+      end
+    end
   end
 end
