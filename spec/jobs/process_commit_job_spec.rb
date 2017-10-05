309063e2f3b6a9dd2661f7bb8bb0d63cd79ad239
Eugen Kuksa <kuksa.eugen@gmail.com>
2017-10-05 12:20:35 +0200

    Send jobs to the HetsAgent.

--- spec/jobs/process_commit_job_spec.rb
+++ spec/jobs/process_commit_job_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe ProcessCommitJob, type: :job do
     let(:repository_id) { 1 }
     let(:commit_sha) { '0' * 40 }
     let(:file_version_parents_creator) { double(:file_version_parents_creator) }
+    let(:hets_agent_invoker) { double(:hets_agent_invoker) }
 
     before do
       # Stub the FileVersionParentsCreator
@@ -18,12 +19,26 @@ RSpec.describe ProcessCommitJob, type: :job do
       allow(file_version_parents_creator).
         to receive(:call)
 
+      # Stub the HetsAgentInvoker
+      allow(HetsAgentInvoker).
+        to receive(:new).
+        with(repository_id, commit_sha).
+        and_return(hets_agent_invoker)
+
+      allow(hets_agent_invoker).
+        to receive(:call)
+
       # Run the job
       ProcessCommitJob.new.perform(repository_id, commit_sha)
     end
 
     it 'calls the FileVersionParentsCreator' do
       expect(file_version_parents_creator).
+        to have_received(:call)
+    end
+
+    it 'calls the HetsAgentInvoker' do
+      expect(hets_agent_invoker).
         to have_received(:call)
     end
   end
