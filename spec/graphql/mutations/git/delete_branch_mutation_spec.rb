9d06834b524487545cefe9f580fe4b117d4d711b
Tom Gehrke <phyrog@cognitive-coding.com>
2017-10-06 09:59:33 +0200

    Authorize git branch deletion mutation

--- spec/graphql/mutations/git/delete_branch_mutation_spec.rb
+++ spec/graphql/mutations/git/delete_branch_mutation_spec.rb
@@ -3,12 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe 'deleteBranch mutation' do
-  let!(:repository) { create :repository_compound, :not_empty }
+  let!(:user) { create :user }
+  let!(:repository) { create :repository_compound, :not_empty, owner: user }
   let(:name) { generate(:branchname) }
   let(:name_argument) { name }
   let(:revision) { 'master' }
 
-  let(:context) {}
+  let(:context) { {current_user: user} }
   let(:variables) do
     {'repositoryId' => repository.to_param,
      'name' => name_argument,
@@ -61,6 +62,19 @@ RSpec.describe 'deleteBranch mutation' do
     it 'does not delete the branch' do
       subject
       expect(repository.git.branch_names).to include(name)
+    end
+
+    context 'because the user is not signed in' do
+      let(:context) { {} }
+
+      it 'returns no data' do
+        expect(subject['data']['commit']).to be(nil)
+      end
+
+      it 'returns an error' do
+        expect(subject['errors']).
+          to include(include('message' => "You're not authorized to do this"))
+      end
     end
   end
 end
