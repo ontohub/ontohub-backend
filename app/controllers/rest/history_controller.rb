f36121209df704c05a8ae33728141ccb94a55ca4
Daniel Hackbarth <da_ha@uni-bremen.de>
2017-10-13 13:52:20 +0200

    obey review

--- app/controllers/rest/history_controller.rb
+++ app/controllers/rest/history_controller.rb
@@ -12,9 +12,9 @@ module Rest
          'path' => params[:path]}
       end
       query <<-QUERY
-        query ($id: ID!, $revision: String) {
+        query ($id: ID!, $revision: String, $limit: Int, $skip: Int, $skipMerges: Boolean, $before: Time, $after: Time) {
           repository(id: $id) {
-            log (revision: $revision) {
+            log (revision: $revision, limit: $limit, skip: $skip, skipMerges: $skipMerges, before: $before, after: $after) {
               id
               message
               committedAt
--- spec/controllers/rest/history_controller_spec.rb
+++ spec/controllers/rest/history_controller_spec.rb
@@ -5,21 +5,26 @@ require 'rails_helper'
 RSpec.describe Rest::HistoryController do
   let!(:repository) { create :repository_compound, :not_empty }
   let!(:user) { repository.owner }
-  let!(:path) { repository.git.ls_files(revision).first }
   let!(:revision) { repository.git.default_branch }
+  let!(:path) { repository.git.ls_files(revision).first }
+  let!(:commits) { repository.git.log(path: path).map(&:id) }
 
   context 'successful (without revision)' do
     describe 'action: index' do
       before do
         get :index, params: {organizational_unit_id: user.to_param,
-                              repository_id:
-                              repository.to_param.sub(%r{\A[^/]*/}, ''),
-                              path: path}
+                             repository_id:
+                               repository.to_param.sub(%r{\A[^/]*/}, ''),
+                             path: path}
       end
       it { expect(response).to have_http_status(:ok) }
       it { |example| expect([example, response]).to comply_with_rest_api }
       it 'finds the repository' do
         expect(response_data['repository']).not_to be(nil)
+      end
+      it 'checks if all commits are present' do
+        expect(response_data['repository']['log'].
+          map { |commit| commit['id'] }).to eq(commits)
       end
     end
   end
@@ -28,15 +33,19 @@ RSpec.describe Rest::HistoryController do
     describe 'action: index' do
       before do
         get :index, params: {organizational_unit_id: user.to_param,
-                            repository_id:
-                              repository.to_param.sub(%r{\A[^/]*/}, ''),
-                            path: path,
-                            revision: revision}
+                             repository_id:
+                               repository.to_param.sub(%r{\A[^/]*/}, ''),
+                             path: path,
+                             revision: revision}
       end
       it { expect(response).to have_http_status(:ok) }
       it { |example| expect([example, response]).to comply_with_rest_api }
       it 'finds the repository' do
         expect(response_data['repository']['log']).not_to be(nil)
+      end
+      it 'checks if all commits are present' do
+        expect(response_data['repository']['log'].
+          map { |commit| commit['id'] }).to eq(commits)
       end
     end
   end
@@ -45,7 +54,7 @@ RSpec.describe Rest::HistoryController do
     describe 'action: index' do
       before do
         get :index, params: {organizational_unit_id: '', repository_id: '',
-                            path: ''}
+                             path: ''}
       end
       it { expect(response).to have_http_status(:ok) }
       it { |example| expect([example, response]).to comply_with_rest_api }
@@ -59,7 +68,7 @@ RSpec.describe Rest::HistoryController do
     describe 'action: index' do
       before do
         get :index, params: {organizational_unit_id: '', repository_id: '',
-                            path: '', revision: ''}
+                             path: '', revision: ''}
       end
       it { expect(response).to have_http_status(:ok) }
       it { |example| expect([example, response]).to comply_with_rest_api }
