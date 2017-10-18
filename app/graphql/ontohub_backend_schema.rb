890890779c357d7fd64352d0297a84150fbd1122
Tom Gehrke <phyrog@users.noreply.github.com>
2017-10-18 14:26:03 +0200

    Simplifies the graphql resolve_type block (#252)

--- app/graphql/ontohub_backend_schema.rb
+++ app/graphql/ontohub_backend_schema.rb
@@ -5,11 +5,10 @@ OntohubBackendSchema = GraphQL::Schema.define do
     if root.respond_to?(:kind)
       "Types::#{root.kind}Type".constantize
     else
-      # If we return objects of class xyzCompound, remove the Compound suffix to
-      # find the correct GraphQL type
-      "Types::#{root.class.to_s.sub(/Compound\z/, '')}Type".constantize
+      "Types::#{root.model_name}Type".constantize
     end
   end)
+
   # The last Instrumenter is executed first, so make sure these are in the
   # correct order
   instrument(:field, Instrumenters::ValidationErrorInstrumenter.new)
