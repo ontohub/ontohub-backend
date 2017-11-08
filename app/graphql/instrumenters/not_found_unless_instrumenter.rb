d28beb729f3b63ebd9c1b6ea369a92af507d7ebd
Tom Gehrke <phyrog@cognitive-coding.com>
2017-11-08 15:24:42 +0100

    Obey hound

--- app/graphql/instrumenters/not_found_unless_instrumenter.rb
+++ app/graphql/instrumenters/not_found_unless_instrumenter.rb
@@ -1,22 +1,20 @@
 # frozen_string_literal: true
 
 module Instrumenters
-  # Rescues Sequel::ValidationFailed errors and raises
-  # GraphQL::ExecutionError instead
+  # Instrumenter that allows returning 'resource not found' based on policies
   class NotFoundUnlessInstrumenter
     # rubocop:disable Metrics/MethodLength
-    # rubocop:disable Metrics/AbcSize
     def instrument(_type, field)
       # rubocop:enable Metrics/MethodLength
-      # rubocop:enable Metrics/AbcSize
       query = field.metadata[:not_found_unless]
       return field unless query
 
       old_resolve = field.resolve_proc
       field.redefine do
         resolve(lambda do |root, arguments, context|
-          if Pundit.policy!(context[:current_user], root).public_send(query.to_s + '?')
-            return old_resolve.call(root, arguments, context) 
+          policy = Pundit.policy!(context[:current_user], root)
+          if policy.public_send(query.to_s + '?')
+            return old_resolve.call(root, arguments, context)
           end
 
           context.add_error(GraphQL::ExecutionError.new('resource not found'))
@@ -28,6 +26,6 @@ module Instrumenters
 end
 
 GraphQL::Field.accepts_definitions(
-  not_found_unless: 
+  not_found_unless:
     GraphQL::Define::InstanceDefinable::AssignMetadataKey.new(:not_found_unless)
 )
