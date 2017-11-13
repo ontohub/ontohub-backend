b34ee4247435f044225d69b99f8e27ecec7abc23
Tom Gehrke <phyrog@cognitive-coding.com>
2017-11-13 11:44:05 +0100

    Initialize Faker with RSpec seed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -66,4 +66,7 @@ RSpec.configure do |config|
   config.fuubar_progress_bar_options = {format: '[%B] %c/%C',
                                         progress_mark: '#',
                                         remainder_mark: '-'}
+
+  # Configure faker to use the RSpec seed
+  Faker::Config.random = Random.new(config.seed)
 end
