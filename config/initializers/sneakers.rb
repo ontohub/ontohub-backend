91f46f4af0bffc817d81b6b0b78cb2f7b165ba58
Eugen Kuksa <kuksa.eugen@gmail.com>
2018-01-07 22:10:57 +0100

    Add RabbitMQ exchange option.

--- config/initializers/sneakers.rb
+++ config/initializers/sneakers.rb
@@ -2,13 +2,15 @@
 
 if Rails.env.test?
   require Rails.root.join('spec/support/bunnymock_recent_history_exchange.rb')
-  Sneakers.configure(connection: BunnyMock.new)
+  Sneakers.configure(connection: BunnyMock.new,
+                     exchange: Settings.rabbitmq.exchange)
 else
   # :nocov:
   Sneakers.configure(connection: Bunny.new(username: Settings.rabbitmq.username,
                                            password: Settings.rabbitmq.password,
                                            host: Settings.rabbitmq.host,
-                                           port: Settings.rabbitmq.port))
+                                           port: Settings.rabbitmq.port),
+                     exchange: Settings.rabbitmq.exchange)
   Sneakers.logger.level = Logger::ERROR
   # :nocov:
 end
