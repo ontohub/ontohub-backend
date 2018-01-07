ceaf07561fafdba92ab6da364aa327e8ac650cb9
Eugen Kuksa <kuksa.eugen@gmail.com>
2018-01-07 22:08:22 +0100

    Add RabbitMQ prefix option.

--- app/workers/mailers_worker.rb
+++ app/workers/mailers_worker.rb
@@ -2,5 +2,5 @@
 
 # Worker for the mailers queue
 class MailersWorker < ApplicationWorker
-  from_queue :mailers, threads: 1, prefetch: 1
+  from_queue "#{Settings.rabbitmq.prefix}_mailers", threads: 1, prefetch: 1
 end
--- app/workers/repository_pulling_worker.rb
+++ app/workers/repository_pulling_worker.rb
@@ -2,5 +2,6 @@
 
 # Worker for the git pull queue
 class RepositoryPullingWorker < ApplicationWorker
-  from_queue :git_pull, threads: 4, prefetch: 1, timeout_job_after: nil
+  from_queue "#{Settings.rabbitmq.prefix}_git_pull",
+    threads: 4, prefetch: 1, timeout_job_after: nil
 end
