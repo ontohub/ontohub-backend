d4b434f758f9fca66e3918ac06c09bc98160b9a6
stickler-ci <support@stickler-ci.com>
2018-03-19 16:34:13 +0000

    Fixing style errors.

--- db/seeds/020_organization_seeds.rb
+++ db/seeds/020_organization_seeds.rb
@@ -2,14 +2,14 @@
 
 Organization.
   create(name: 'seed-user-organization',
-      display_name: 'Seed User Organization',
-      description: 'All users that are created in the seeds').tap do |org|
+         display_name: 'Seed User Organization',
+         description: 'All users that are created in the seeds').tap do |org|
   IndexingJob.perform_later('class' => 'Organization', 'id' => org.id)
 end
 
 Organization.
   create(name: 'the-league-of-extraordinary-users',
-      display_name: 'The League of Extraordinary Users',
-      description: 'Really fabulous, wow.').tap do |org|
+         display_name: 'The League of Extraordinary Users',
+         description: 'Really fabulous, wow.').tap do |org|
   IndexingJob.perform_later('class' => 'Organization', 'id' => org.id)
 end
