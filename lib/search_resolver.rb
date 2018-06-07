d29bf598bc60fb2503541192f48d711038176431
Daniel Hackbarth <da_ha@uni-bremen.de>
2018-06-07 12:08:43 +0200

    GraphQL SearchResult implemented

--- lib/search_resolver.rb
+++ lib/search_resolver.rb
@@ -19,8 +19,57 @@ class SearchResolver
     result = result.flatten
     graphQLResult = OpenStruct.new(
       global: OpenStruct.new(
-        
+        entries: createEntries(result)
+        count: OpenStruct.new(
+          all: result.size,
+          organizationalUnits: allOrganizationalUnits(result),
+          repositories: allRepositories(result)
+        )
       )
     )
-d  end
+  end
+
+  private
+  def allOrganizationalUnits(result)
+    search_result = 0
+    result.each do |element|
+      elem = element._data["_index"]
+      if elem == 'user' || elem == 'organization'
+        search_result+=1
+      end
+    end
+    search_result
+  end
+
+  private
+  def allRepositories(result)
+    search_result = 0
+    result.each do |element|
+      elem = element._data["_index"]
+      if elem == 'repository'
+        search_result+=1
+      end
+    end
+    search_result
+  end
+
+  private
+  def createEntries(result)
+    entries = []
+    result.each do |element|
+      entries.append(
+        OpenStruct.new(
+          ranking: element._data["score"]
+          entry: if element._data["_type"] == 'user'
+            User.first(slug: element.attributes["slug"])
+          elsif element._data["_type"] == 'repository'
+            Repostiroy.first(slug: element.attributes["slug"])
+          else
+            Organization.first(slug: element.attributes["slug"])
+          end
+        )
+      )
+    end
+    entries
+  end
 end
